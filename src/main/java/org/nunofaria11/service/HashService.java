37816da7f8d500d9d99c71a0a374d9fafe7faa01
nunofaria11 <2145649+nunofaria11@users.noreply.github.com>
2024-09-23 02:44:05 +0100

    feat: configurable hash length

--- src/main/java/org/nunofaria11/service/HashService.java
+++ src/main/java/org/nunofaria11/service/HashService.java
@@ -1,6 +1,7 @@
 package org.nunofaria11.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.Random;
 
@@ -9,11 +10,13 @@ public class HashService {
 
     private static final Random RANDOM = new Random();
     private static final String CHARS = "abcdefghijklmnopqrstuvwxyz0123456789";
-    private static final int LENGTH = 6;
+
+    @ConfigProperty(name = "url-shortener.hash-length", defaultValue = "6")
+    int hashLength;
 
     public String generate() {
-        final StringBuilder builder = new StringBuilder(LENGTH);
-        for (int i = 0; i < LENGTH; i++) {
+        final StringBuilder builder = new StringBuilder(hashLength);
+        for (int i = 0; i < hashLength; i++) {
             builder.append(CHARS.charAt(RANDOM.nextInt(CHARS.length())));
         }
         return builder.toString();
